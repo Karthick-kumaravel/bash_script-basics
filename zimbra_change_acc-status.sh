0bd5ef03b0ece6d1af07690ce054370746e72b2b
karthick-kumaravel <kaarthick.kumaravel@gmail.com>
2017-07-26 15:57:00 +0530

    script

--- zimbra_change_acc-status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in `cat locked.txt`
-do
-`zmprov ma $i zimbraAccountStatus locked`
-echo "$i account closed" >> results.txt
-done
-exit 0
--- zimbra_importcontacts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in `cat mailid.txt`
-do
-	a=$(ls contact/ | grep "$i")
-	`zmmailbox -z -m $i pru /Contacts contact/$a`
-	echo "Contact imported to $i"
-done
